31c6dcfbe91953e7faf29afe66b4a81d28df45b8
zhangyu <grewer@grewer.cn>
2020-03-09 19:29:13 +0800

    webview 渲染成功

--- src/WebViewTest.tsx
+++ src/WebViewTest.tsx
@@ -81,8 +81,13 @@ class WebViewTest extends React.Component {
                  source={{ uri: DEFAULT_URL }}
                  startInLoadingState={true}
                  domStorageEnabled={true}
+                 thirdPartyCookiesEnabled={true}
                  javaScriptEnabled={true}
-                 allowsInlineMediaPlayback={true}
+                 mixedContentMode={'always'}
+                 androidHardwareAccelerationDisabled={true}
+                 allowUniversalAccessFromFileURLs={true}
+                 cacheEnabled={true}
+                 userAgent={`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.34 Safari/537.36"`}
                  renderError={errorName => <View><Text>{errorName}</Text></View>}
         >
         </WebView>
