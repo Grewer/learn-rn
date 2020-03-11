0af3166895e83ea4b60113e076e0cb1eacaa096f
zhangyu <grewer@grewer.cn>
2020-03-11 22:22:23 +0800

    control 优化 考虑是否要删除 changeMoveTime

--- src/components/control.tsx
+++ src/components/control.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import Util from '../utils/util'
 import Orientation from 'react-native-orientation-locker'
 import Slider from '@react-native-community/slider'
@@ -21,6 +21,20 @@ const TotalTime: React.FC<{ duration: number }> = React.memo((props) => {
     color: '#fff',
     fontSize: 12
   }}>{` / ${Util.formSecondTotHMS(props.duration)}`}</Text>
+})
+
+const StartAndPaused: React.FC<Pick<IProps, 'changePaused' | 'paused'>> = React.memo((props) => {
+  return <TouchableOpacity
+    onPress={props.changePaused}
+    style={{
+      height: '100%',
+      width: 50,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+    <Image style={{ height: 25, width: 25 }}
+           source={props.paused ? require('../images/play.png') : require('../images/pause.png')}/>
+  </TouchableOpacity>
 })
 
 
@@ -53,72 +67,23 @@ class Control extends React.Component<IProps> {
     const time = moveTime ? moveTime : currentTime
     console.log('render control', moveTime, time)
     return (
-      <View style={{
-        width: '100%',
-        height: 60,
-        position: 'absolute',
-        bottom: 0,
-        left: controlShow ? 0 : -1000,
-        backgroundColor: 'rgba(0,0,0,0.5)'
-      }}>
-        <View style={{
-          flexDirection: 'row',
-          width: '100%',
-          height: '50%',
-          backgroundColor: 'rgba(0,0,0,0)'
-        }}>
-
-          <Slider
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '100%',
-            }}
-            thumbImage={require('../images/icon_control_slider.png')}
-            thumbTintColor="#ffffff"
-            minimumValue={0}
-            step={1}
-            value={moveTime}
-            maximumValue={duration}
-            onValueChange={this.changeMoveTime}
-            onSlidingComplete={this.complete}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="#999"
-          />
-        </View>
-
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          height: '50%',
-          backgroundColor: 'rgba(0,0,0,0)'
-        }}>
-
-          <View style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
-            alignItems: 'center',
-            flex: 1,
-            height: '100%',
-          }}>
-
-            <TouchableOpacity
-              onPress={() => {
-                console.log('Play or Paused', paused)
-                changePaused()
-              }}
-              style={{
-                height: '100%',
-                width: 50,
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}>
-              <Image style={{ height: 25, width: 25 }}
-                     source={paused ? require('../images/play.png') : require('../images/pause.png')}/>
-            </TouchableOpacity>
-
+      <View style={[styles.container, { left: controlShow ? 0 : -1000 }]}>
+        <Slider
+          style={styles.slider}
+          thumbImage={require('../images/icon_control_slider.png')}
+          thumbTintColor="#ffffff"
+          minimumValue={0}
+          step={1}
+          value={time}
+          maximumValue={duration}
+          onValueChange={this.changeMoveTime}
+          onSlidingComplete={this.complete}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#999"
+        />
+        <View style={styles.tools}>
+          <View style={styles.toolLeft}>
+            <StartAndPaused paused={paused} changePaused={changePaused}/>
             <Text style={{
               color: '#fff',
               fontSize: 12
@@ -126,48 +91,41 @@ class Control extends React.Component<IProps> {
             <TotalTime duration={duration}/>
           </View>
 
-          <View style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            flex: 1,
-            height: '100%',
-            backgroundColor: 'rgba(0,0,0,0)'
-          }}>
+          <View style={styles.toolRight}>
 
-            <TouchableOpacity
-              onPress={() => {
-                this.setState({
-                  rateShow: true
-                })
-              }}
-              style={{
-                height: '100%',
-                width: 50,
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}>
-              <Text
-                style={{ color: '#fff' }}>{rate == 1 ? '倍速' : rate + 'x'}</Text>
-            </TouchableOpacity>
+            {/*<TouchableOpacity*/}
+            {/*  onPress={() => {*/}
+            {/*    this.setState({*/}
+            {/*      rateShow: true*/}
+            {/*    })*/}
+            {/*  }}*/}
+            {/*  style={{*/}
+            {/*    height: '100%',*/}
+            {/*    width: 50,*/}
+            {/*    justifyContent: 'center',*/}
+            {/*    alignItems: 'center'*/}
+            {/*  }}>*/}
+            {/*  <Text*/}
+            {/*    style={{ color: '#fff' }}>{rate == 1 ? '倍速' : rate + 'x'}</Text>*/}
+            {/*</TouchableOpacity>*/}
 
-            <TouchableOpacity
-              onPress={() => {
-                if (isPortrait) {
-                  Orientation.lockToLandscapeRight()
-                } else {
-                  Orientation.lockToPortrait()
-                }
-              }}
-              style={{
-                height: '100%',
-                width: 50,
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}>
-              <Image style={{ height: 25, width: 25 }}
-                     source={require('../images/bigscreen.png')}/>
-            </TouchableOpacity>
+            {/*<TouchableOpacity*/}
+            {/*  onPress={() => {*/}
+            {/*    if (isPortrait) {*/}
+            {/*      Orientation.lockToLandscapeRight()*/}
+            {/*    } else {*/}
+            {/*      Orientation.lockToPortrait()*/}
+            {/*    }*/}
+            {/*  }}*/}
+            {/*  style={{*/}
+            {/*    height: '100%',*/}
+            {/*    width: 50,*/}
+            {/*    justifyContent: 'center',*/}
+            {/*    alignItems: 'center'*/}
+            {/*  }}>*/}
+            {/*  <Image style={{ height: 25, width: 25 }}*/}
+            {/*         source={require('../images/bigscreen.png')}/>*/}
+            {/*</TouchableOpacity>*/}
 
           </View>
 
@@ -176,5 +134,44 @@ class Control extends React.Component<IProps> {
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 60,
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  slider: {
+    width: '100%',
+    height: '50%',
+    backgroundColor: 'rgba(0,0,0,0)'
+  },
+  tools: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '50%',
+    backgroundColor: 'rgba(0,0,0,0)'
+  },
+  toolLeft: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 1,
+    height: '100%',
+  },
+  toolRight: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flex: 1,
+    height: '100%',
+    backgroundColor: 'rgba(0,0,0,0)'
+  }
+})
 
 export default Control
